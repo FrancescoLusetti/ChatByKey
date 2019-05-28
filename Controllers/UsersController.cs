7ef512600d565bd8d145503c755a880c5a63cf54
Francesco Lusetti <0002luso.ciccio@gmail.com>
2019-05-28 13:13:51 +0200

    First Commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiBroker.Helper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBroker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Users/SignUp
+        [HttpPost("SignUp")]
+        public JsonResult SignUp([FromForm] string username, [FromForm] string password)
+        {
+            UsersHelper.InsertUser("dope", "swag");
+            return Json();
+        }
+
+        // POST api/Users/Login
+        [HttpPost("Login")]
+        public JsonResult Login([FromForm] string username, [FromForm] string password)
+        {
+            UsersHelper.Login("dope", "swag");
+            return Json();
+        }
+    }
+}
---
+++ Helper/UsersHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBroker.Helper
+{
+    public class UsersHelper
+    {
+        public static bool InsertUser(string username, string password)
+        {
+            return true;
+        }
+
+        public static bool Login(string username, string password)
+        {
+            return true;
+        }
+    }
+}
